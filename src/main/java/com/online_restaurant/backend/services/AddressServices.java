b1cf91ca5e1c81a36b4b034541fa043e08f22717
Saman <saman_mehr_ali_poor@tutanota.com>
2024-10-18 20:09:03 +0330

    service created for address

--- src/main/java/com/online_restaurant/backend/services/AddressServices.java
+++ src/main/java/com/online_restaurant/backend/services/AddressServices.java
@@ -1,4 +1,47 @@
 package com.online_restaurant.backend.services;
 
+
+import com.online_restaurant.backend.exception.IligalEntityException;
+import com.online_restaurant.backend.model.Address;
+import com.online_restaurant.backend.model.User;
+import com.online_restaurant.backend.repository.AddressRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AddressServices {
+
+    @Autowired
+    private AddressRepo addressRepo;
+
+    public Address save(Address address){
+        if (address.getUser() == null){
+            throw  new IligalEntityException("user is null");
+
+        }
+        addressRepo.saveAddress(address);
+        return address;
+    }
+
+
+
+    public List<Address> getAll(){
+        return addressRepo.getAllAddress();
+    }
+
+    public List<Address> getAll(User user){
+        return addressRepo.getAllAddress(user);
+    }
+
+    public boolean remove(Address address){
+//        Address address = new Address();
+        return addressRepo.remove(address);
+    }
+
+
+    public boolean Update(Address address){
+        return addressRepo.update(address);
+    }
 }
